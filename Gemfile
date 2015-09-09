1d92b292db4b7ad9ab88ed039141ad3aad152c08
Louis Bacon <louis@homeflow.co.uk>
2015-09-09 14:10:57 +0100

    Posts and admin

--- Gemfile
+++ Gemfile
@@ -28,6 +28,8 @@ gem 'spring',        group: :development
 
 gem 'haml-rails'
 
+gem 'devise'
+
 # Use ActiveModel has_secure_password
 # gem 'bcrypt', '~> 3.1.7'
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
-  resources :posts
+  resources :posts, only: [:index, :show]
+  
+  namespace :admin do
+    resources :posts
+  end
 
   root 'welcome#index'
 
